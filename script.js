1f4f381e387ef38f15a4a9b0ea23a04b01d699e1
reloZid <relozid@logixisland.ch>
2019-11-24 14:38:16 +0100

    use parceljs bundler

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import './style.css'
+
+import 'bootstrap'
+import $ from 'jquery'
+
 let settings = {
   fretboard: {
     tuning: ['E', 'B', 'G', 'D', 'A', 'E'],
